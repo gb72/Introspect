4265b32078634340e57089c8da0ee2a3745526df
Gina Bradshaw <gb72@st-andrews.ac.uk>
2017-11-13 17:53:55 +0000

    added lightbox gallery and bootstrap navbar

--- Introspect.js
+++ Introspect.js
@@ -1,64 +1,56 @@
-var images =[];
-var index = 0;
+// var images =[];
+// var index = 0;
 
 
-function rightchangeimage()
-{
-	var img = document.getElementById("photo");
-	var over = document.getElementById("overimg");
-	
-	index=index+1;
-
-	if(index >= images.length){
-		index=0;
-	}
-	img.src = images[index];
-	over.src = images[index];
-}
-function leftchangeimage()
-{
-	var img= document.getElementById("photo");
-	var over=document.getElementById("overimg");
-	index=index-1;
-
-	if(index <0){
-		index=images.length-1;
-	}
-	img.src = images[index];
-	over.src = images[index];
-}
-
-
-images[0]="IntrospectPhoto1.jpg";
-images[1]="IntrospectPhoto2.jpg";
-images[2]="IntrospectPhoto3.jpg";
-images[3]="IntrospectPhoto4.jpg";
-images[4]="IntrospectPhoto5.jpg";
-images[5]="IntrospectPhoto6.jpg";
-images[6]="IntrospectPhoto7.jpg";
-
-
-
-document.getElementById('buttonboxprevious').onclick = leftchangeimage;
-document.getElementById('buttonboxnext').onclick = rightchangeimage;
-
-
-function on() {
-    document.getElementById("overlay").style.display = "block";
-}
-
-function off() {
-    document.getElementById("overlay").style.display = "none";
-}
-
-
-document.getElementById('photo').onclick= on;
-document.getElementById('overlay').onclick= off;
-// function enlargeimage()
+// function rightchangeimage()
 // {
-// 	var img = getElementById('photo')
-// 	img.width=1000px;
-// 	img.height=auto;
+// 	var img = document.getElementById("photo");
+// 	var over = document.getElementById("overimg");
+	
+// 	index=index+1;
+
+// 	if(index >= images.length){
+// 		index=0;
+// 	}
+// 	img.src = images[index];
+// 	over.src = images[index];
+// }
+// function leftchangeimage()
+// {
+// 	var img= document.getElementById("photo");
+// 	var over=document.getElementById("overimg");
+// 	index=index-1;
+
+// 	if(index <0){
+// 		index=images.length-1;
+// 	}
+// 	img.src = images[index];
+// 	over.src = images[index];
 // }
 
-// document.getElementById('photo').onclick = enlargeimage;
+
+// images[0]="IntrospectPhoto1.jpg";
+// images[1]="IntrospectPhoto2.jpg";
+// images[2]="IntrospectPhoto3.jpg";
+// images[3]="IntrospectPhoto4.jpg";
+// images[4]="IntrospectPhoto5.jpg";
+// images[5]="IntrospectPhoto6.jpg";
+// images[6]="IntrospectPhoto7.jpg";
+
+
+
+// document.getElementById('buttonboxprevious').onclick = leftchangeimage;
+// document.getElementById('buttonboxnext').onclick = rightchangeimage;
+
+
+// function on() {
+//     document.getElementById("overlay").style.display = "block";
+// }
+
+// function off() {
+//     document.getElementById("overlay").style.display = "none";
+// }
+
+
+// document.getElementById('photo').onclick= on;
+// document.getElementById('overlay').onclick= off;
